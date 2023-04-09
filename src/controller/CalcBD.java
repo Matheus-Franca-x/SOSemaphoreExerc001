27e7b212d4549178531c89fa59bf3d483ef73f8e
matheus <matheussss6613@gmail.com>
2023-04-09 11:56:35 -0300

    fim

--- src/controller/CalcBD.java
+++ src/controller/CalcBD.java
@@ -1,9 +1,107 @@
 package controller;
 
-public class CalcBD 
+import java.util.concurrent.Semaphore;
+
+public class CalcBD extends Thread
 {
-	public CalcBD()
+	private int id;
+	private Semaphore fila;
+	
+	public CalcBD(int id, Semaphore fila)
 	{
-		//Estou em outro estado, não vou conseguir fazer agora :x
+		this.id = id;
+		this.fila = fila;
+	}
+	
+	public void bancoDeDados()
+	{
+		new Thread()
+		{
+			public void run()
+			{
+				iniBD();
+				
+			}
+		}.start();
+	}
+	
+	public void iniBD()
+	{
+		if(id % 3 == 1)
+		{
+			calc(1);
+		}
+		else if(id % 3 == 2)
+		{
+			calc(2);
+		}
+		else
+		{
+			calc(0);
+		}
+	}
+	
+	public void calc(int tipo)
+	{
+		try {
+			if(tipo == 1)
+			{
+				for (int i = 0; i < 2; i++)
+				{
+					System.out.println("O id " + this.id + " do tipo " + tipo + " está calculando");
+					sleep((int)(Math.random() * 801) + 200);
+					BD(tipo);
+				}
+			}
+			else if(tipo == 2)
+			{
+				for (int i = 0; i < 3; i++)
+				{					
+					System.out.println("O id " + this.id + " do tipo " + tipo + " está calculando");
+					sleep((int)(Math.random() * 1001) + 500);
+					BD(tipo);
+				}
+			}
+			else 
+			{
+				for (int i = 0; i < 3; i++)
+				{					
+					System.out.println("O id " + this.id + " do tipo " + tipo + " está calculando");
+					sleep((int)(Math.random() * 1001) + 1000);
+					BD(tipo);
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("O id " + this.id + " do tipo " + tipo + " terminou sua tarefa.");
+	}
+	
+	public void BD(int tipo)
+	{
+		try {
+			if(tipo == 1)
+			{
+				fila.acquire();
+				System.out.println("O id " + this.id + " do tipo " + tipo + " esta fazendo transacao no BD");
+				sleep(1000);			
+			}
+			else if(tipo == 2)
+			{
+				fila.acquire();
+				System.out.println("O id " + this.id + " do tipo " + tipo + " esta fazendo transacao no BD");
+				sleep(1500);
+			}
+			else
+			{
+				fila.acquire();
+				System.out.println("O id " + this.id + " do tipo " + tipo + " esta fazendo transacao no BD");
+				sleep(1500);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			fila.release();
+		}
 	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,21 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.CalcBD;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
+		
+		Semaphore perm = new Semaphore(1);
+		
+		for(int i = 1; i <= 21; i++)
+		{
+			CalcBD controllBD = new CalcBD(i, perm);
+			controllBD.bancoDeDados();
+		}
 		
 	}
 
